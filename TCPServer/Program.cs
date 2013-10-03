b2446a7343312c33a06731c0e47e50e79da9c41c
MarekZajac <contact@zajacmarek.com>
2013-10-03 10:38:15 +0200

    first commit

---
+++ TCPServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace TCPServer
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Info("Application start");
+            logger.Info("Application terminated");
+        }
+    }
+}
---
+++ TCPServer/Server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using NLog;
+
+namespace TCPServer
+{
+    class Server
+    {
+        private Logger logger = LogManager.GetCurrentClassLogger();
+
+        private TcpListener listener;
+        private TcpClient client;
+
+        public Server()
+        {
+            logger.Info("Server object was created");
+
+            int port = ReadPort();
+
+            listener = new TcpListener(IPAddress.Any, port);
+            client = default(TcpClient);
+        }
+
+        public void Run()
+        {
+            logger.Info("Server run");
+
+            listener.Start();
+
+            while (true)
+            {
+                client = listener.AcceptTcpClient();
+
+                SingleConnection connection = new SingleConnection(client);
+            }
+        }
+
+        private int ReadPort()
+        {
+            int port;
+            Console.WriteLine("Port:");
+            if (!Int32.TryParse(Console.ReadLine(), out port))
+            {
+                port = 8888;
+            }
+
+            return port;
+        }
+    }
+}
